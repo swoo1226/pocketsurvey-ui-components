a9d0676b34064cd74f61cb71fbfe715847a2e175
pp.jo <pp.jo@earlysloth.com>
2021-02-06 17:44:37 +0900

    폰트 파일 추가

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = async ({ config, mode }) => {
+  config.module.rules.push(
+    {
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../'),
+    },
+    {
+      test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+      loaders: ['file-loader'],
+      include: path.resolve(__dirname, '../'),
+    },
+  );
+  return config;
+};
