e335d6eabfbb8a39d4652224507d29cf149a253e
pp.jo <pp.jo@earlysloth.com>
2021-02-06 17:44:52 +0900

    addon knobs 추가

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,12 @@
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
-}
+  addons: [
+    '@storybook/addon-a11y',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-knobs/register',
+  ],
+};
