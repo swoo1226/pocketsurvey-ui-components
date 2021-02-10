b08fb0a668d81694a47086a9dd6bc71ff46e3128
shin-earlysloth <shin@earlysloth.com>
2021-02-10 10:57:31 +0900

    Textarea 스토리북 추가

---
+++ stories/Textarea/Textarea.stories.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { withKnobs, text, select, color } from '@storybook/addon-knobs';
+
+import { Textarea } from './Textarea';
+
+export default {
+    title: 'Core/Textarea', // 스토리북에서 보여질 그룹과 경로를 명시
+    component: Textarea, // 어떤 컴포넌트를 문서화 할지 명시
+    decorators: [withKnobs], // 애드온 적용
+};
+
+export const Index = () => {
+    const type = select('타입', ['basic', 'line'], 'basic');
+    const size = select('사이즈', ['small', 'medium', 'big'], 'small');
+    const children = text('텍스트', '가나다라마바사아자차카타파하');
+    return (
+        <Textarea type={type} size={size}>
+            {children}
+        </Textarea>
+    );
+};
+
+Index.story = {
+    name: 'Default',
+};
