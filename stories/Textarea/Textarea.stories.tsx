ea455b17b3c2c7298f4a802c336130efc8a6b761
pp-ops <pp.jo@earlysloth.com>
2021-02-12 15:32:33 +0900

    textarea 컴포넌트 위치 변경

--- stories/Textarea/Textarea.stories.tsx
+++ stories/Textarea/Textarea.stories.tsx
@@ -4,7 +4,7 @@ import { withKnobs, text, select, color } from '@storybook/addon-knobs';
 import { Textarea } from './Textarea';
 
 export default {
-    title: 'Core/Textarea', // 스토리북에서 보여질 그룹과 경로를 명시
+    title: 'Components/Textarea', // 스토리북에서 보여질 그룹과 경로를 명시
     component: Textarea, // 어떤 컴포넌트를 문서화 할지 명시
     decorators: [withKnobs], // 애드온 적용
 };
