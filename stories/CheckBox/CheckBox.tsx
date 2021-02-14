eb838c242425f96ed35ca2a2302ca930d5a4a1c0
pp-ops <pp.jo@earlysloth.com>
2021-02-14 16:29:51 +0900

    checkbox 컴포넌트 완성

---
+++ stories/CheckBox/CheckBox.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import styled from 'styled-components';
+import checkImage from './check.svg';
+
+const CheckBoxContainer = styled.div``;
+const CheckBoxList = styled.div``;
+const CheckBoxItem = styled.div`
+  display: flex;
+  margin-bottom: 14px;
+`;
+const CheckBoxSelectionLabel = styled.label`
+  margin-left: 14px;
+`;
+const CheckBoxSelectionItem = styled.input`
+  margin: 0;
+  opacity: 0;
+  width: 17px;
+  height: 17px;
+  all: unset;
+  padding-left: 17px;
+  border: 0;
+  border-radius: 3px;
+  border: 1px solid #dfdedd;
+  &:hover {
+    border: 1px solid #f2ab28;
+  }
+  &:checked {
+    padding-left: 11px;
+    border: 4px solid #f2ab28;
+    background-color: #f2ab28;
+    background-image: url(${checkImage});
+    background-repeat: no-repeat;
+  }
+`;
+
+export type CheckBoxType = {
+  name: string;
+  selections: {
+    label: string;
+  }[];
+  selected: string[];
+  onItemClick: (index: number) => void;
+  className?: string;
+};
+
+const CheckBox = ({
+  name,
+  selections,
+  selected,
+  onItemClick,
+  className,
+}: CheckBoxType) => {
+  return (
+    <CheckBoxContainer className={className}>
+      <CheckBoxList>
+        {selections.map((item, index) => {
+          return (
+            <CheckBoxItem onClick={() => onItemClick(index)}>
+              <CheckBoxSelectionItem
+                type="checkbox"
+                name={name}
+                checked={selected.includes(index.toString())}
+              />
+              <CheckBoxSelectionLabel htmlFor={name}>
+                {item.label}
+              </CheckBoxSelectionLabel>
+            </CheckBoxItem>
+          );
+        })}
+      </CheckBoxList>
+    </CheckBoxContainer>
+  );
+};
+
+export default CheckBox;
