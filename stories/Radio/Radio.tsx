1266063781ca0b4163d45828e4fce6d1374eff65
pp-ops <pp.jo@earlysloth.com>
2021-02-14 16:10:35 +0900

    라디오 컴포넌트 개발 완료

---
+++ stories/Radio/Radio.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const RadioContainer = styled.div``;
+const RadioList = styled.div``;
+const RadioItem = styled.div`
+  display: flex;
+  margin-bottom: 14px;
+`;
+const RadioSelectionLabel = styled.label`
+  margin-left: 14px;
+`;
+const RadioSelectionItem = styled.input`
+  margin: 0;
+  opacity: 0;
+  width: 17px;
+  height: 17px;
+  all: unset;
+  padding-left: 17px;
+  border: 0;
+  border-radius: 100%;
+  border: 1px solid #dfdedd;
+  &:hover {
+    border: 1px solid #f2ab28;
+  }
+  &:checked {
+    padding-left: 11px;
+    border: 4px solid #f2ab28;
+  }
+`;
+
+export type RadioType = {
+  name: string;
+  selections: {
+    label: string;
+  }[];
+  selected: number;
+  onItemClick: (index: number) => void;
+  className?: string;
+};
+
+const Radio = ({
+  name,
+  selections,
+  selected,
+  onItemClick,
+  className,
+}: RadioType) => {
+  return (
+    <RadioContainer className={className}>
+      <RadioList>
+        {selections.map((item, index) => {
+          return (
+            <RadioItem onClick={() => onItemClick(index)}>
+              <RadioSelectionItem
+                type="radio"
+                name={name}
+                checked={index == selected}
+              />
+              <RadioSelectionLabel htmlFor={name}>
+                {item.label}
+              </RadioSelectionLabel>
+            </RadioItem>
+          );
+        })}
+      </RadioList>
+    </RadioContainer>
+  );
+};
+
+export default Radio;
