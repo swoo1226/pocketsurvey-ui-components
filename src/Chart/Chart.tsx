9f36eb678a327653243daa3dbba6c01fc8dde818
shin-earlysloth <shin@earlysloth.com>
2021-02-19 15:42:50 +0900

    차트 컴포넌트 빈 형태로 제작

---
+++ src/Chart/Chart.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+type BarTypes =
+  | 'bar-horizontal-analysis-report'
+  | 'bar-horizontal-stacked-analysis-report';
+
+//bar-horizontal-analysis-report => 인공지능 보고서에서 사용하는 가로 바 차트
+//bar-horizontal-stacked-analysis-report => 인공지능 보고서에서 사용하는 가로 바 차트. 데이터를 스택 형태로 쌓음.
+
+type PieTypes = 'pie-analysis-report';
+
+//pie-analysis-report => 인공지능 보고서에서 사용하는 파이 차트
+
+type EChartTypes = BarTypes & PieTypes;
+
+type ChartType = {
+  type: EChartTypes;
+};
+
+const ChartRouter = (type: EChartTypes) => {
+  switch (type) {
+    case 'bar-horizontal-analysis-report':
+      return <React.Fragment>bar-horizontal-analysis-report</React.Fragment>;
+  }
+};
+
+const Chart = ({ type }: ChartType) => {
+  return <React.Fragment>{ChartRouter(type)}</React.Fragment>;
+};
+
+export default Chart;
