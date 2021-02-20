20a68f1394045cc3508983c7609c9f751611c072
pp-ops <pp.jo@earlysloth.com>
2021-02-20 16:07:15 +0900

    토글 컴포넌트 추가

---
+++ src/Toggle/Toggle.stories.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { withKnobs, color } from '@storybook/addon-knobs';
+
+import Toggle from './Toggle';
+
+export default {
+  title: 'Components/Toggle', // 스토리북에서 보여질 그룹과 경로를 명시
+  component: Toggle, // 어떤 컴포넌트를 문서화 할지 명시
+  decorators: [withKnobs], // 애드온 적용
+};
+
+export const Index = () => {
+  const [isToggleOn, setIsToggleOn] = useState<boolean>(false);
+
+  const toggleOnBackgroundColor = color(
+    'Toggle on background-color',
+    '#59c4db',
+  );
+  const hoveredBackgroundColor = color('Hover on background-color', '#818282');
+
+  return (
+    <Toggle
+      toggleOnBackgroundColor={toggleOnBackgroundColor}
+      hoveredBackgroundColor={hoveredBackgroundColor}
+      isToggleOn={isToggleOn}
+      setIsToggleOn={setIsToggleOn}
+    />
+  );
+};
---
+++ src/Toggle/Toggle.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ToggleContainer = styled.div<{
+  backgroundColor: string;
+  hoveredBackgroundColor: string;
+  isToggleOn: boolean;
+}>`
+  display: flex;
+  align-items: center;
+  background-color: ${(props) => props.backgroundColor};
+  width: 50px;
+  height: 30px;
+  cursor: pointer;
+  transition: 0.1s;
+  border-radius: 50rem;
+  position: relative;
+  &:hover {
+    ${(props) =>
+      !props.isToggleOn
+        ? `background-color: ${props.hoveredBackgroundColor}`
+        : ''};
+  }
+`;
+const ToggleButton = styled.div<{ isToggleOn: boolean }>`
+  width: 24px;
+  height: 24px;
+  transition: 0.2s;
+  background-color: #fff;
+  border-radius: 50%;
+  position: absolute;
+  ${(props) => (props.isToggleOn ? 'left : 23px;' : 'left: 3px;')}
+`;
+
+type ToggleType = {
+  toggleOnBackgroundColor: string;
+  hoveredBackgroundColor: string;
+  isToggleOn: boolean;
+  setIsToggleOn: (isToggleOn: boolean) => void;
+};
+
+const Toggle = ({
+  toggleOnBackgroundColor,
+  hoveredBackgroundColor,
+  isToggleOn,
+  setIsToggleOn,
+}: ToggleType) => {
+  return (
+    <ToggleContainer
+      backgroundColor={isToggleOn ? toggleOnBackgroundColor : '#d6d6d6'}
+      hoveredBackgroundColor={hoveredBackgroundColor}
+      isToggleOn={isToggleOn}
+      onClick={() => setIsToggleOn(!isToggleOn)}
+    >
+      <ToggleButton isToggleOn={isToggleOn} />
+    </ToggleContainer>
+  );
+};
+
+export default Toggle;
