6a000c1d707232fd54a68966a6ff12aef1b08644
peter.kim <sangwoo.kim@earlysloth.com>
2021-02-23 14:09:08 +0900

    pagination component 구현

--- src/Pagination/Pagination.stories.tsx
+++ src/Pagination/Pagination.stories.tsx
@@ -1,23 +1,49 @@
 import React from 'react';
-import { withKnobs } from '@storybook/addon-knobs';
+import { withKnobs, select, boolean, color } from '@storybook/addon-knobs';
 
-import Pagination, { PaginationType } from './Pagination';
+import PaginationComponent, { PaginationType } from './Pagination';
 import { Meta } from '@storybook/react/types-6-0';
 
 export default {
-  component: Pagination,
+  component: PaginationComponent,
   title: 'Components/Pagination',
   decorators: [withKnobs],
 } as Meta;
 
 export const Index = () => {
+  const size = select(
+    'size',
+    ['normal','small','large'],
+    'normal'
+  )
+  const count = select(
+    'count', [1,7,8,10], 8
+  )
+  const disabled = boolean('disabled', false)
+  const defaultPage = select('defaultPage', [1,3], 1)
+  const siblingCount = select(
+    'siblingCount', [1,3,4], 1
+  )
+  const boundaryCount = select(
+    'boundaryCount', [1,3,4], 1
+  )
+  const selectedBackgroundColor = color('selected background color', '#FAC62D');
+  const selectedTextColor = color('selected text color', '#FFFFFF');
+  const hoveredBackgroundColor = color('hovered background color', '#F0F0F0');
+  const hoveredTextColor = color('hovered text color', '#2B2E33');
   return (
-    <Pagination
-      count={10}
-      selected={3}
-      theme="primary"
-      onClickNext={() => alert('onClickNext')}
-      onClickPrev={() => alert('onClickPrev')}
+    <PaginationComponent
+      count={count}
+      size={size}
+      defaultPage={defaultPage}
+      disabled={disabled}
+      siblingCount={siblingCount}
+      boundaryCount={boundaryCount}
+      onChange={(e)=>{alert(e.target.textContent)}}
+      selectedBackgroundColor={selectedBackgroundColor}
+      hoveredBackgroundColor={hoveredBackgroundColor}
+      selectedTextColor={selectedTextColor}
+      hoveredTextColor={hoveredTextColor}
     />
   );
 };
--- src/Pagination/Pagination.tsx
+++ src/Pagination/Pagination.tsx
@@ -1,34 +1,101 @@
 import React from 'react';
 import styled from 'styled-components';
-
+import {makeStyles} from '@material-ui/core/styles'
+import Pagination from '@material-ui/lab/Pagination'
 import Icon from '../Icon/Icon';
 
 const PaginationContainer = styled.div``;
 
-type ThemeType = 'primary' | 'secondary' | 'tertiary';
+type SizeType = 'normal' | 'small' | 'large';
 
 export type PaginationType = {
   count: number;
-  selected: number;
-  theme: ThemeType;
-  onClickNext: () => void;
-  onClickPrev: () => void;
+  size: SizeType;
+  defaultPage: number;
+  onChange: (e: React.ChangeEvent) => void;
+  disabled: boolean;
+  siblingCount: number;
+  boundaryCount: number;
+  selectedBackgroundColor: string;
+  hoveredBackgroundColor: string;
+  selectedTextColor: string;
+  hoveredTextColor: string;
 };
 
-const Pagination = ({
+
+
+const PaginationComponent = ({
   count,
-  selected,
-  theme,
-  onClickNext,
-  onClickPrev,
+  size,
+  disabled,
+  defaultPage,
+  onChange,
+  siblingCount,
+  boundaryCount,
+  selectedBackgroundColor,
+  hoveredBackgroundColor,
+  selectedTextColor,
+  hoveredTextColor
 }: PaginationType) => {
+  const useStyles = makeStyles({
+    root: {
+      '& > *': {
+        // marginTop: theme.spacing(2),
+        color: '#2B2E33',
+      },
+      '& .MuiPaginationItem-page:hover': {
+        backgroundColor: hoveredBackgroundColor,
+        color: hoveredTextColor
+      },
+      '& .Mui-selected': {
+        backgroundColor: selectedBackgroundColor,
+        color: selectedTextColor,
+      },
+      '& .Mui-selected:hover': {
+        backgroundColor: '#FAC62D',
+        color: selectedTextColor,
+      },
+    },
+  });
+  const classes = useStyles();
   return (
-    <PaginationContainer>
-      <Icon icon="paginationArrow" width={50} rotate={0} />
-
-      <Icon icon="paginationArrow" width={50} rotate={180} />
-    </PaginationContainer>
+    <div className={classes.root}>
+      <Pagination
+        count={count}
+        defaultPage={defaultPage}
+        siblingCount={siblingCount}
+        boundaryCount={boundaryCount}
+        size={size != 'normal' ? size : undefined}
+        onChange={(e) => {
+          onChange(e);
+        }}
+        disabled={disabled}
+      />
+      {/* <Pagination
+        count={8}
+        defaultPage={1}
+        siblingCount={1}
+        boundaryCount={1}
+        color="primary"
+      />
+      <Pagination
+        count={8}
+        defaultPage={1}
+        siblingCount={1}
+        boundaryCount={1}
+        color="secondary"
+        size={size != 'normal' ? size : undefined}
+      />
+      <Pagination count={3} disabled /> */}
+    </div>
   );
+  // return (
+  //   <PaginationContainer>
+  //     <Icon icon="paginationArrow" width={50} rotate={0} />
+
+  //     <Icon icon="paginationArrow" width={50} rotate={180} />
+  //   </PaginationContainer>
+  // );
 };
 
-export default Pagination;
+export default PaginationComponent;
