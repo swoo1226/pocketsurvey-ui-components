f8e84309e422287ea4aada136f82055645025061
ellie-hahm <ellie.hahm@earlysloth.com>
2021-02-24 10:51:35 +0900

    Lint 추가

--- src/Textarea/Textarea.test.tsx
+++ src/Textarea/Textarea.test.tsx
@@ -1,33 +1,33 @@
-import React from "react";
-import { render, RenderResult, waitFor } from '@testing-library/react';
-import "@testing-library/jest-dom";
-import Textarea from './Textarea';
+import React from "react"
+import { render, RenderResult, waitFor } from "@testing-library/react"
+import "@testing-library/jest-dom"
+import Textarea from "./Textarea"
 import userEvent from "@testing-library/user-event"
 
-describe('Textarea Test', () => {
-    var renderResult: RenderResult;
-    beforeEach(() => {
-        renderResult = render(
-            <Textarea
-            type={"basic"}
-            size={"small"}
-            width={"100"}
-            >
-            </Textarea>
-        )
-    });
+describe("Textarea Test", () => {
+  let renderResult: RenderResult
+  beforeEach(() => {
+    renderResult = render(
+      <Textarea
+        type={"basic"}
+        size={"small"}
+        width={"100"}
+      >
+      </Textarea>
+    )
+  })
 
-    it('Rendering Test', () => {
-        const { container } = renderResult;
-        expect(container);
-    });
+  it("Rendering Test", () => {
+    const { container } = renderResult
+    expect(container)
+  })
 
-    it("textarea focus 테스트", async() => {
-        const { getByTestId } = renderResult;
-        const txtArea = getByTestId("textarea-testid")
-        userEvent.click(txtArea)
+  it("textarea focus 테스트", async() => {
+    const { getByTestId } = renderResult
+    const txtArea = getByTestId("textarea-testid")
+    userEvent.click(txtArea)
         
-        await waitFor(()=>expect(txtArea).toHaveFocus())
+    await waitFor(()=>expect(txtArea).toHaveFocus())
 
-    })
-});
+  })
+})
