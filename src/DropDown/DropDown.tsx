7545d90c83ac739bc483db66591ea60e5b7ae460
pp.jo <pp.jo@earlysloth.com>
2021-03-11 14:56:39 +0900

    Revert "사용하지 않는 useState 제거"
    This reverts commit 4d021b51dbd208f10e399e0cf34280d0a96c2c2c.

--- src/DropDown/DropDown.tsx
+++ src/DropDown/DropDown.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react"
+import React, { useState, useEffect, useRef } from "react"
 import styled from "styled-components"
 
 import Icon from "../Icon/Icon"
