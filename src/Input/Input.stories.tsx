d6867f5b0f612b4549ad62807f8f8e831e03437a
pp.jo <pp.jo@earlysloth.com>
2021-03-12 17:18:49 +0900

    input 수정

--- src/Input/Input.stories.tsx
+++ src/Input/Input.stories.tsx
@@ -1,43 +1,56 @@
 import React, { useState } from "react"
-import {
-	withKnobs,
-	number,
-	boolean,
-	text,
-	select,
-} from "@storybook/addon-knobs"
+import { withKnobs, number, boolean, text, select } from "@storybook/addon-knobs"
 
 import Input from "./Input"
 import { Meta } from "@storybook/react/types-6-0"
 
 export default {
-	component: Input,
-	title: "Components/Input",
-	decorators: [withKnobs],
+  component: Input,
+  title: "Components/Input",
+  decorators: [withKnobs],
 } as Meta
 
 export function Index() {
-	const [value, setValue] = useState<string>("")
+  const [value, setValue] = useState<string>("")
 
-	const width = number("width", 300)
-	const placeholder = text("placeholder", "텍스트를 입력해주세요.")
-	const errorMessage = text("errorMessage", "에러메시지")
-	const disabled = boolean("disabled", false)
-	const useCancleButton = boolean("useCancleButton", false)
-	const isError = boolean("isError", false)
-	const mode = select("mode", ["line", "basic"], "line")
+  const width = number("width", 300)
+  const placeholder = text("placeholder", "텍스트를 입력해주세요.")
+  const errorMessage = text("errorMessage", "에러메시지")
+  const disabled = boolean("disabled", false)
+  const useCancleButton = boolean("useCancleButton", false)
+  const isError = boolean("isError", false)
+  const mode = select("mode", ["line", "basic"], "line")
+  const readOnly = boolean("readOnly", false)
+  const tabIndex = number("tabIndex", 10)
 
-	return (
-		<Input
-			mode={mode}
-			placeholder={placeholder}
-			value={value}
-			onChange={(value: string) => setValue(value)}
-			width={width}
-			isError={isError}
-			errorMessage={errorMessage}
-			disabled={disabled}
-			useCancleButton={useCancleButton}
-		/>
-	)
+  return (
+    <Input
+      mode={mode}
+      placeholder={placeholder}
+      value={value}
+      onChange={(value: string) => setValue(value)}
+      width={width}
+      isError={isError}
+      errorMessage={errorMessage}
+      disabled={disabled}
+      useCancelButton={useCancleButton}
+      readOnly={readOnly}
+      tabIndex={tabIndex}
+      onFocus={() => {
+        console.log("onFocus")
+      }}
+      onClick={() => {
+        console.log("onClick")
+      }}
+      onBlur={() => {
+        console.log("onBlur")
+      }}
+      onClickCancelButton={() => {
+        console.log("onClickCancelButton")
+      }}
+      onKeyDown={() => {
+        console.log("onKeyDown")
+      }}
+    />
+  )
 }
