a6f472ae3b1b96d12721bb3c68a47abf62f48ceb
peter.kim <sangwoo.kim@earlysloth.com>
2021-04-08 15:27:05 +0900

    드롭다운 열릴 때의 스타일 수정

--- src/DropDown/DropDown.stories.tsx
+++ src/DropDown/DropDown.stories.tsx
@@ -1,54 +1,76 @@
-import React, { useState } from "react"
-import { withKnobs, boolean, select, color } from "@storybook/addon-knobs"
+import React, { useState } from "react";
+import { withKnobs, boolean, select, color } from "@storybook/addon-knobs";
 
-import DropDown from "./DropDown"
-import { Meta } from "@storybook/react/types-6-0"
+import DropDown from "./DropDown";
+import { Meta } from "@storybook/react/types-6-0";
 
 export default {
   component: DropDown,
   title: "Components/DropDown",
   decorators: [withKnobs], // 애드온 적용
-} as Meta
+} as Meta;
 
 export function TypeSelector() {
-  const [selected, setSelected] = useState(0)
+  const [selected, setSelected] = useState(0);
 
-  const disabled = boolean("disabled", false)
+  const disabled = boolean("disabled", false);
 
-  const mainColor = color("bold color", "#FAC62D")
-  const subColor = color("light color", "#fef4ce")
+  const mainColor = color("bold color", "#FAC62D");
+  const subColor = color("light color", "#fef4ce");
 
   return (
-    <DropDown
-      list={[
-        {
-          selectionName: "객관식 (단일 선택)",
-          icon: "singleChoice",
-        },
-        { selectionName: "객관식 (복수 선택)", icon: "singleChoice" },
-        { selectionName: "객관식 (이미지 선택)", icon: "singleChoice" },
-        { selectionName: "주관식 (텍스트)", icon: "singleChoice" },
-        { selectionName: "주관식 (이미지)", icon: "singleChoice" },
-        { selectionName: "객관식 (영상)", icon: "singleChoice" },
-        { selectionName: "설명 추가", icon: "singleChoice", hidden: true },
-        { selectionName: "순위 설정", icon: "singleChoice" },
-      ]}
-      selected={selected}
-      disable={disabled}
-      themeColor={{ mainColor, subColor }}
-      onItemClick={(index: number) => setSelected(index)}
-      iconColor="#FAC62D"
-    />
-  )
+    <>
+      <DropDown
+        list={[
+          {
+            selectionName: "객관식 (단일 선택)",
+            icon: "singleChoice",
+          },
+          { selectionName: "객관식 (복수 선택)", icon: "singleChoice" },
+          { selectionName: "객관식 (이미지 선택)", icon: "singleChoice" },
+          { selectionName: "주관식 (텍스트)", icon: "singleChoice" },
+          { selectionName: "주관식 (이미지)", icon: "singleChoice" },
+          { selectionName: "객관식 (영상)", icon: "singleChoice" },
+          { selectionName: "설명 추가", icon: "singleChoice", hidden: true },
+          { selectionName: "순위 설정", icon: "singleChoice" },
+        ]}
+        selected={selected}
+        disable={disabled}
+        themeColor={{ mainColor, subColor }}
+        onItemClick={(index: number) => setSelected(index)}
+        iconColor="#FAC62D"
+      />
+      <DropDown
+        list={[
+          {
+            selectionName: "객관식 (단일 선택)",
+            icon: "singleChoice",
+          },
+          { selectionName: "객관식 (복수 선택)", icon: "singleChoice" },
+          { selectionName: "객관식 (이미지 선택)", icon: "singleChoice" },
+          { selectionName: "주관식 (텍스트)", icon: "singleChoice" },
+          { selectionName: "주관식 (이미지)", icon: "singleChoice" },
+          { selectionName: "객관식 (영상)", icon: "singleChoice" },
+          { selectionName: "설명 추가", icon: "singleChoice", hidden: true },
+          { selectionName: "순위 설정", icon: "singleChoice" },
+        ]}
+        selected={selected}
+        disable={disabled}
+        themeColor={{ mainColor, subColor }}
+        onItemClick={(index: number) => setSelected(index)}
+        iconColor="#FAC62D"
+      />
+    </>
+  );
 }
 
 export function BranchSelector() {
-  const [selected, setSelected] = useState(null)
+  const [selected, setSelected] = useState(null);
 
-  const disabled = boolean("disabled", false)
+  const disabled = boolean("disabled", false);
 
-  const mainColor = color("bold color", "#59C4DB")
-  const subColor = color("light color", "#DEF3F8")
+  const mainColor = color("bold color", "#59C4DB");
+  const subColor = color("light color", "#DEF3F8");
 
   return (
     <DropDown
@@ -71,5 +93,5 @@ export function BranchSelector() {
       //   onItemClick={(index: number) => alert(`${index + 1}번째 아이템 클릭`)}
       onItemClick={(index: number) => setSelected({ group: 1, item: index })}
     />
-  )
+  );
 }
--- src/DropDown/DropDown.tsx
+++ src/DropDown/DropDown.tsx
@@ -1,16 +1,20 @@
-import React, { useState, useEffect, useRef } from "react"
-import styled from "styled-components"
+import React, { useState, useEffect, useRef } from "react";
+import styled from "styled-components";
 
-import Icon, { IconType } from "../Icon/Icon"
+import Icon, { IconType } from "../Icon/Icon";
 
-const DropDownContainer = styled.div`
-  width: 200px;
+const DropDownContainer = styled.div<{
+  width: number;
+}>`
+  width: ${(props) => props.width}px;
   font-size: 12px;
-`
+  text-align: left;
+`;
 
 const DropDownBoxContainer = styled.div<{
-  disable: boolean
-  themeColor: string
+  disable: boolean;
+  themeColor: string;
+  zIndex: number;
 }>`
   width: 100%;
   display: flex;
@@ -21,44 +25,58 @@ const DropDownBoxContainer = styled.div<{
   border: 1px solid ${(props) => (props.disable ? "#DFDEDD" : props.themeColor)};
   background-color: ${(props) => (props.disable ? "#F0F0F0" : "white")};
   cursor: ${(props) => (props.disable ? "no-drop" : "pointer")};
-`
+  z-index: ${(props) => props.zIndex - 1};
+`;
 const DropDownBox = styled.div<{
-  disable: boolean
-  themeColor: string
+  disable: boolean;
+  themeColor: string;
+  height: number;
 }>`
   width: 80%;
-  height: 34px;
+  height: ${(props) => props.height}px;
   display: flex;
   align-items: center;
   color: ${(props) => (props.disable ? "#818282" : "#111111")};
   background-color: ${(props) => (props.disable ? "#F0F0F0" : "white")};
   cursor: ${(props) => (props.disable ? "no-drop" : "pointer")};
-`
+`;
 const DropDownList = styled.div<{
-  disable: boolean
-  isShowList: boolean
+  disable: boolean;
+  isShowList: boolean;
+  listLength: number;
+  width: number;
+  zIndex: number;
 }>`
-  width: 100%;
+  width: ${(props) => props.width}px;
+  z-index: ${(props) => props.zIndex};
+  position: absolute;
   box-shadow: 0px 3px 6px #d2cbc0;
   color: ${(props) => (props.disable ? "#818282" : "#111111")};
   border-radius: 3px;
   padding: 8px 0;
-`
+  max-height: 200px;
+  overflow-y: auto;
+  background: #ffffff;
+`;
 const DropDownItem = styled.div<{
-  index: number
-  selected: number
-  themeColor: string
-  hidden?: boolean
+  index: number;
+  selected: number;
+  themeColor: string;
+  hidden?: boolean;
+  height: number;
 }>`
   display: ${(props) => (props.hidden ? "none" : "flex")};
   align-items: center;
   box-sizing: border-box;
   padding: 0 10px;
-  background-color: ${(props) => (props.selected == props.index ? "#F0F0F0" : "white")};
+  height: ${(props) => props.height}px;
+  background-color: ${(props) =>
+    props.selected == props.index ? "#F0F0F0" : "white"};
   &:hover {
-    background-color: ${(props) => (props.selected == props.index ? "#F0F0F0" : props.themeColor)};
+    background-color: ${(props) =>
+      props.selected == props.index ? "#F0F0F0" : props.themeColor};
   }
-`
+`;
 const DropDownItemText = styled.p`
   white-space: nowrap;
   overflow: hidden;
@@ -67,78 +85,124 @@ const DropDownItemText = styled.p`
   margin: 0;
   margin-left: 6px;
   text-overflow: ellipsis;
-`
+`;
 
 export type DropDownType = {
   list: {
-    selectionName: string
-    icon?: IconType
-    hidden?: boolean
-  }[]
-  iconColor?: string
-  selected: number | null
-  disable: boolean
+    selectionName: string;
+    icon?: IconType;
+    hidden?: boolean;
+  }[];
+  iconColor?: string;
+  selected: number | null;
+  disable: boolean;
   themeColor: {
-    mainColor: string
-    subColor: string
-  }
-  onItemClick: (index: number) => void
-  className?: string
-  placeholder?: string
-}
+    mainColor: string;
+    subColor: string;
+  };
+  onItemClick: (index: number) => void;
+  className?: string;
+  placeholder?: string;
+  height?: number;
+  width?: number;
+  zIndex?: number;
+};
 
-function DropDown({ list, selected, disable, themeColor, onItemClick, className, iconColor, placeholder }: DropDownType): JSX.Element {
-  const [isShowList, setIsShowList] = useState<boolean>(false)
-  const selectionListRef = useRef<HTMLDivElement>(null)
+function DropDown({
+  list,
+  selected,
+  disable,
+  themeColor,
+  onItemClick,
+  className,
+  iconColor,
+  placeholder,
+  height = 34,
+  width = 200,
+  zIndex = 20,
+}: DropDownType): JSX.Element {
+  const [isShowList, setIsShowList] = useState<boolean>(false);
+  const selectionListRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     /**
      * Alert if clicked on outside of element
      */
     function handleClickOutside(event: any) {
-      if (selectionListRef.current && !selectionListRef.current.contains(event.target)) {
-        setIsShowList(false)
+      if (
+        selectionListRef.current &&
+        !selectionListRef.current.contains(event.target)
+      ) {
+        setIsShowList(false);
       }
     }
 
     // Bind the event listener
-    document.addEventListener("mousedown", handleClickOutside)
+    document.addEventListener("mousedown", handleClickOutside);
     return () => {
       // Unbind the event listener on clean up
-      document.removeEventListener("mousedown", handleClickOutside)
-    }
-  }, [selectionListRef])
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [selectionListRef]);
 
   return (
-    <DropDownContainer ref={selectionListRef} className={className}>
+    <DropDownContainer
+      ref={selectionListRef}
+      className={className}
+      width={width}
+    >
       <DropDownBoxContainer
         onClick={() => {
-          setIsShowList(!isShowList)
+          setIsShowList(!isShowList);
         }}
         disable={disable}
         themeColor={themeColor.mainColor}
+        zIndex={zIndex}
       >
         <DropDownBox
           onClick={() => {
-            setIsShowList(!isShowList)
+            setIsShowList(!isShowList);
           }}
           disable={disable}
           themeColor={themeColor.mainColor}
           data-testid="dropdownbox-testid"
+          height={height}
         >
           {selected !== null ? (
             <React.Fragment>
-              {list[selected].icon && <Icon color={disable ? "#818282" : iconColor!} icon={list[selected].icon!} width={18} />}
-              <DropDownItemText>{list[selected].selectionName}</DropDownItemText>
+              {list[selected].icon && (
+                <Icon
+                  color={disable ? "#818282" : iconColor!}
+                  icon={list[selected].icon!}
+                  width={18}
+                />
+              )}
+              <DropDownItemText>
+                {list[selected].selectionName}
+              </DropDownItemText>
             </React.Fragment>
           ) : (
             <DropDownItemText>{placeholder}</DropDownItemText>
           )}
         </DropDownBox>
-        <Icon color={disable ? "#818282" : iconColor!} icon="arrow" useCursor={disable ? false : true} width={18} rotate={!isShowList ? 90 : 270} />
+        <Icon
+          color={disable ? "#818282" : iconColor!}
+          icon="arrow"
+          useCursor={disable ? false : true}
+          width={18}
+          rotate={!isShowList ? 90 : 270}
+        />
       </DropDownBoxContainer>
 
-      <DropDownList isShowList={isShowList} disable={disable} style={!isShowList ? { display: "none" } : { opacity: "1" }} data-testid="dropdownlist-testid">
+      <DropDownList
+        isShowList={isShowList}
+        disable={disable}
+        style={!isShowList ? { display: "none" } : { opacity: "1" }}
+        data-testid="dropdownlist-testid"
+        listLength={list.length}
+        width={width}
+        zIndex={zIndex}
+      >
         {list.map((item, index) => (
           <DropDownItem
             key={index}
@@ -146,18 +210,25 @@ function DropDown({ list, selected, disable, themeColor, onItemClick, className,
             selected={selected!}
             themeColor={themeColor.subColor}
             onClick={() => {
-              onItemClick(index)
-              setIsShowList(false)
+              onItemClick(index);
+              setIsShowList(false);
             }}
+            height={height}
             hidden={item.hidden}
           >
-            {item.icon && <Icon color={disable ? "#818282" : iconColor!} icon={item.icon} width={18} />}
+            {item.icon && (
+              <Icon
+                color={disable ? "#818282" : iconColor!}
+                icon={item.icon}
+                width={18}
+              />
+            )}
             <DropDownItemText>{item.selectionName}</DropDownItemText>
           </DropDownItem>
         ))}
       </DropDownList>
     </DropDownContainer>
-  )
+  );
 }
 
-export default DropDown
+export default DropDown;
