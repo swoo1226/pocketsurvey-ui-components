561c174024c4a20671ffa24179e91654a8ed4e8d
shin-earlysloth <shin@earlysloth.com>
2021-05-28 18:21:09 +0900

    스택 차트 export

--- src/Chart/options/index.ts
+++ src/Chart/options/index.ts
@@ -2,6 +2,7 @@
 import barHorizontalBaseOption from "./barHorizontalBase";
 import barVerticalBaseOption from "./barVerticalBase";
 import BarVerticalStacked from "./barVerticalStacked"
+import barHorizontalStacked from "./barHorizontalStacked"
 import lineBaseOption from "./lineBase";
 import PieBaseOption from "./pieBase";
 import defaultOption from "./defaultOption";
@@ -9,6 +10,7 @@ import defaultOption from "./defaultOption";
 export { barHorizontalBaseOption };
 export { barVerticalBaseOption };
 export {BarVerticalStacked}
+export {barHorizontalStacked}
 export { lineBaseOption };
 export {PieBaseOption}
 export { defaultOption };
