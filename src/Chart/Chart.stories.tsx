6bb8965e21c558f8c6b37ad07e96b8175d1b15c5
shin-earlysloth <shin@earlysloth.com>
2021-05-28 15:42:56 +0900

    렌더링 및 스토리북 추가

--- src/Chart/Chart.stories.tsx
+++ src/Chart/Chart.stories.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { withKnobs, boolean, text } from "@storybook/addon-knobs";
 
-import Chart from './Chart'
+import Chart from "./Chart";
 import { Meta } from "@storybook/react/types-6-0";
 
 export default {
@@ -10,24 +10,94 @@ export default {
   decorators: [withKnobs],
 } as Meta;
 
-export function BarHorizontalBase() { 
+export function BarHorizontalBase() {
   return (
     <Chart
-        type={"bar-horizontal-base"}
-        labels={["인터넷 검색", "네이버 블로그", "브런치", "페이스북 페이지", "지인 소개", "뉴스기사"]}
-        series={[74, 24, 5, 2, 23, 5, 2]}
-    />  
+      type={"bar-horizontal-base"}
+      labels={[
+        "인터넷 검색",
+        "네이버 블로그",
+        "브런치",
+        "페이스북 페이지",
+        "지인 소개",
+        "뉴스기사",
+      ]}
+      series={[74, 24, 5, 2, 23, 5, 2]}
+    />
   );
 }
 
-
-export function BarVerticalBase() { 
+export function BarVerticalBase() {
   return (
     <Chart
-        type={"bar-vertical-base"}
-        labels={["한글한글한글한글한글한글한글한글한글한글한글한글한글한글한글한글한글한글한글한글", "네이버네이버네이버네이버네이버네이버 블로그", "브런치", "페이스북 페이지", "지인 소개", "뉴스기사"]}
-        series={[74, 24, 5, 0, 23, 5, 2]}
-        height={34}
-    />  
+      type={"bar-vertical-base"}
+      labels={[
+        "한글한글한글한글한글한글한글한글한글한글한글한글한글한글한글한글한글한글한글한글",
+        "네이버네이버네이버네이버네이버네이버 블로그",
+        "브런치",
+        "페이스북 페이지",
+        "지인 소개",
+        "뉴스기사",
+      ]}
+      series={[74, 24, 5, 0, 23, 5, 2]}
+    />
+  );
+}
+
+export function BarVerticalStacked() {
+  return (
+    <Chart
+      type={"bar-vertical-stacked"}
+      labels={[
+        "인터넷 검색",
+        "네이버 블로그",
+        "브런치",
+        "페이스북 페이지",
+        "지인 소개",
+        "뉴스",
+        "기타:",
+      ]}
+      series={[
+        [50, 57.9, 63, 50, 37.5, 50, 50, null, 100],
+        [null, 15.8, 11.1, 16.7, null, null, null, 100, null],
+        [null, null, 3.7, 5.6, 12.5, null, null, null, null],
+        [null, 5.3, null, null, null, null, null, null, null],
+        [null, 15.8, 18.5, 16.7, 37.5, 50, 50, null, null],
+        [50, null, 3.7, 5.6, 12.5, null, null, null, null],
+        [null, 5.3, null, 5.6, null, null, null, null, null],
+      ]}
+      xAxisLabel={[
+        "2019-11-17",
+        "2019-11-24",
+        "2019-12-01",
+        "2019-12-08",
+        "2019-12-15",
+        "2019-12-22",
+        "2019-12-29",
+        "2020-01-05",
+        "2020-01-12",
+      ]}
+    />
+  );
+}
+
+export function LineBase() {
+  return (
+    <Chart
+      type={"line-base"}
+      labels={["매우 만족함", "만족함", "보통", "불만족함", "매우 불만족함"]}
+      series={[109, 650, 626, 3619, 3483].reverse()}
+      labelOption="fixed"
+    />
+  );
+}
+
+export function PieBase() {
+  return (
+    <Chart
+      type={"pie-base"}
+      labels={["매우 만족함", "만족함", "보통", "불만족함", "매우 불만족함"]}
+      series={[109, 650, 626, 3619, 3483]}
+    />
   );
 }
--- src/Chart/Chart.tsx
+++ src/Chart/Chart.tsx
@@ -1,58 +1,125 @@
-import React, {useState, useRef, useEffect} from "react"
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+
+import React, { useState, useRef, useEffect } from "react"
 import EChartsReact from "echarts-for-react"
 import { EChartsOption } from "echarts"
-import { barHorizontalBaseOption, barVerticalBaseOption } from "./options"
+import {
+  barHorizontalBaseOption,
+  barVerticalBaseOption,
+  BarVerticalStacked,
+  lineBaseOption,
+  PieBaseOption
+} from "./options"
 import styled from "styled-components"
-import {barHorizontalBase, barVerticalBase} from "./types"
-
-type BarTypes =
-  | "bar-horizontal-base"
-  | "bar-vertical-base"
-
-type PieTypes = "pie-analysis-report";
-
-type EChartTypes = BarTypes | PieTypes;
+import { ChartPropsType } from "./types" 
+import {getSizeCSS} from "./util" 
 
 const EchartsReactWrapper = styled.div``
+ 
 
-type ChartType = barHorizontalBase | barVerticalBase
-
-function Chart({ type, width, height, override, labels, series }: ChartType) {
-  const dom = useRef<HTMLDivElement>(null)
+function Chart({
+  type,
+  width,
+  height,
+  override,
+  labels,
+  series,
+  hasMarker,
+  smooth,
+  labelOption,
+  xAxisLabel
+}: ChartPropsType) {
+  const wrapperDom = useRef<HTMLDivElement>(null)
+  const echartDom = useRef<EChartsReact>(null)
+  const [echartInstance, setEchartInstance] = useState<any>(null)
   const [lineWidth, setLineWidth] = useState<number | null>(null)
-  
-  useEffect(()=>{
-    const svg = dom.current?.querySelector("svg > g:last-child > path") as SVGSVGElement
-    setLineWidth(svg?.getBBox()?.width)
-  },[dom])
+  const [option, setOption] = useState<EChartsOption>({})
 
-  switch (type){
+
+  useEffect(() => {
+    if(type === "bar-vertical-base"){
+      const svg = wrapperDom.current?.querySelector(
+        "svg > g:last-child > path"
+      ) as SVGSVGElement
+      setLineWidth(svg?.getBBox()?.width)
+    }
+  }, [wrapperDom])
+
+  switch (type) {
   case "bar-horizontal-base":
-    return  <EChartsReact
-      option={barHorizontalBaseOption({
-        series,
-        labels,
-      })}
-    />
-
-  case "bar-vertical-base": 
-    return <EchartsReactWrapper ref={dom}>
+    return (
       <EChartsReact
-        option={barVerticalBaseOption({
+        style={getSizeCSS(width, height)}
+        option={barHorizontalBaseOption({
           series,
           labels,
-          lineWidth
+          override,
         })}
-        opts={{renderer: "svg"}} 
       />
-    </EchartsReactWrapper>
+    )
 
+  case "bar-vertical-base":
+    return (
+      <EchartsReactWrapper ref={wrapperDom}>
+        <EChartsReact
+          style={getSizeCSS(width, height)}
+          option={barVerticalBaseOption({
+            series,
+            labels,
+            lineWidth,
+            override,
+          })}
+          opts={{ renderer: "svg" }}
+        />
+      </EchartsReactWrapper>
+    )
 
+  case "line-base": 
+    return (
+      <EChartsReact
+        style={getSizeCSS(width, height)}
+        option={lineBaseOption({
+          series: (series as number[]),
+          labels,
+          hasMarker: hasMarker ?? false,
+          smooth: smooth ?? false,
+          labelOption: labelOption ?? "dynamic",
+          override,
+        })}
+      />
+    )
+
+  case "pie-base":
+    return (
+      <EChartsReact
+        style={getSizeCSS(width, height)}
+        option={PieBaseOption({
+          series,
+          labels,
+          override
+        })}
+      />
+    )
+
+  case "bar-vertical-stacked":
+    return (
+      <EChartsReact
+        style={getSizeCSS(width, height)}
+        option={
+          BarVerticalStacked({
+            series: (series as (number|null)[][]),
+            labels, 
+            xAxisLabel: (xAxisLabel as string[]), 
+            override
+          })
+        }
+      />
+    )
 
   default:
-    return (
-      <></>
-    )
+    return <React.Fragment />
   }
 }
 
