3317d728d053f716c6259091cfb902f9df5478ab
shin-earlysloth <shin@earlysloth.com>
2021-05-31 13:42:08 +0900

    차트 문서 작성

--- src/Chart/Chart.stories.tsx
+++ src/Chart/Chart.stories.tsx
@@ -10,7 +10,7 @@ export default {
   decorators: [withKnobs],
 } as Meta;
 
-export function BarHorizontalBase() {
+export function BarHorizontalBase() { 
   return (
     <Chart.BarHorizontalBase 
       labels={[
