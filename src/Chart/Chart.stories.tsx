916a31a006ce9cf43cc8475a27f130acf7f62151
shin-earlysloth <shin@earlysloth.com>
2021-05-31 17:28:52 +0900

    차트, 차트 스토리 추가

--- src/Chart/Chart.stories.tsx
+++ src/Chart/Chart.stories.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { withKnobs, boolean, text } from "@storybook/addon-knobs";
+import { withKnobs, boolean, number, select } from "@storybook/addon-knobs";
 
 import Chart from "./Chart";
 import { Meta } from "@storybook/react/types-6-0";
@@ -43,13 +43,16 @@ export function BarVerticalBase() {
 }
 
 export function LineBase() {
+  const labelOption = select('mode', ['fixed', 'dynamic'], 'dynamic')
+  const hasMarker = boolean("hasMarker", false)
+  const smooth = boolean("smooth", false)
   return (
     <Chart.LineBase
       labels={["매우 만족함", "만족함", "보통", "불만족함", "매우 불만족함"]}
       series={[109, 650, 626, 3619, 3483].reverse()}
-      labelOption="dynamic"
-      hasMarker={true}
-      smooth={true}
+      labelOption={labelOption}
+      hasMarker={hasMarker}
+      smooth={smooth}
     />
   );
 }
@@ -121,3 +124,52 @@ export function PieBase() {
 // }
 
 
+<<<<<<< Updated upstream
+=======
+export function BarVerticalStacked() { 
+  return (
+    <Chart.BarVerticalStacked
+      labels={[
+        "인터넷 검색",
+        "네이버 블로그",
+        "브런치",
+        "페이스북 페이지",
+        "지인 소개",
+        "뉴스",
+        "기타:",
+      ]}
+      series={[
+        [50, 57.9, 63, 50, 37.5, 50, 50, null, 100],
+        [null, 15.8, 11.1, 16.7, null, null, null, 100, null],
+        [null, null, 3.7, 5.6, 12.5, null, null, null, null],
+        [null, 5.3, null, null, null, null, null, null, null],
+        [null, 15.8, 18.5, 16.7, 37.5, 50, 50, null, null],
+        [50, null, 3.7, 5.6, 12.5, null, null, null, null],
+        [null, 5.3, null, 5.6, null, null, null, null, null],
+      ]}
+      xAxisLabel={[
+        "날짜: 2019-11-17",
+        "날짜: 2019-11-24",
+        "날짜: 2019-12-01",
+        "날짜: 2019-12-08",
+        "날짜: 2019-12-15",
+        "날짜: 2019-12-22",
+        "날짜: 2019-12-29",
+        "날짜: 2020-01-05",
+        "날짜: 2020-01-12",
+      ]}
+      line={[
+        {
+          name: "구간 평균 점수",
+          series: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+        },
+        {
+          name: "전체 평균 점수",
+          series: Array.from({ length: 9 }).fill(15) as number[]
+        },
+      ]}
+      height={700} 
+    />
+  );
+}
+>>>>>>> Stashed changes
--- src/Chart/Chart.tsx
+++ src/Chart/Chart.tsx
@@ -2,9 +2,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 
-import React, { useState, useRef, useEffect } from "react"
-import EChartsReact from "echarts-for-react"
-import { EChartsOption } from "echarts"
+import React from "react"
 import {
   BarHorizontalBase,
   BarVerticalBase,
@@ -13,13 +11,7 @@ import {
   LineBase,
   PieBase
 } from "./charts"
-import styled from "styled-components"
-import { ChartPropsType } from "./types" 
-import {getSizeCSS} from "./util" 
-
-const EchartsReactWrapper = styled.div``
- 
-
+import styled from "styled-components"  
 function Chart() {
   return <></>
   // switch (type) {
@@ -62,7 +54,15 @@ function Chart() {
 
 Chart.BarHorizontalBase = BarHorizontalBase
 Chart.BarVerticalBase = BarVerticalBase
+<<<<<<< Updated upstream
 Chart.LineBase = LineBase
+=======
+Chart.BarHorizontalStacked = BarHorizontalStacked
+Chart.BarVerticalStacked = BarVerticalStacked
+
+Chart.LineBase = LineBase
+
+>>>>>>> Stashed changes
 Chart.PieBase = PieBase
 
 export default Chart
