8660a8760a29eb80d614b28dd97b3e1b4c7f1bb9
shin-earlysloth <shin@earlysloth.com>
2021-06-04 15:33:38 +0900

    파이차트, 스택차트에서 사용할 툴팁 함수 추가

--- src/Chart/util/tooltip.ts
+++ src/Chart/util/tooltip.ts
@@ -1,5 +1,46 @@
+import {getMaxLabelWidth} from "./index"
+
 export const sumReducer = (accumulator: number, currentValue: null | number) =>
   accumulator + (currentValue ?? 0)
+
+export const verticalStackedFormatter = (
+  params: {
+      dataIndex: number;
+      seriesIndex: number;
+      marker: string;
+      seriesName: string;
+      data:
+        | {
+            value: number | null;
+          }
+        | number
+        | null;
+      axisValueLabel: string;
+    }[], 
+  series: (number |null)[][]
+) => {
+  const maxLabelWidth = getMaxLabelWidth(params.map((item) => item.seriesName))
+  
+  const row = params.map((param) => { 
+    const tooltipValue = series[param.seriesIndex][param.dataIndex]
+    const value = `${tooltipValue ? tooltipValue : "-"}`
+    const _percent = typeof param.data === "number" ? param.data : param.data?.value
+    const percent = _percent ? `${_percent}%` : "0%"
+    return `<div style="display: flex; justify-content: space-between;">
+        <div style="width: ${Math.ceil(maxLabelWidth) + 10}px; text-align: left;">
+          ${param.marker}
+          <span>${param.seriesName}</span>
+        </div>
+        <span style="font-weight:700;">${value} (${percent})</span>
+        </div>`
+  })
+  return `
+      <div style="text-align: left;">
+    <span>${params[0].axisValueLabel}</span>
+      ${row.join("")}
+      </div>
+    `
+}
 
 export const piePercentageFormatter = (
   params: {
