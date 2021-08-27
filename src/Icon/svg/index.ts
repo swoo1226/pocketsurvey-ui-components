41590fdf808afa06356abcd16786f1681fad8aa6
jjun <jjun@earlysloth.com>
2021-08-27 17:48:16 +0900

    icon export문 추가

--- src/Icon/svg/index.ts
+++ src/Icon/svg/index.ts
@@ -1,3 +1,4 @@
+
 /* eslint-disable semi */
 export { ReactComponent as chartLine } from "./100Chartline.svg";
 export { ReactComponent as arrow } from "./arrow.svg";
@@ -107,3 +108,6 @@ export { ReactComponent as kakaoChannel } from "./kakaoChannel.svg";
 export { ReactComponent as edit } from "./edit.svg";
 export { ReactComponent as sort } from "./sort.svg";
 export { ReactComponent as duplicate } from "./duplicate.svg";
+export {ReactComponent as duplicateSandbox} from "./duplicate_sandbox.svg"
+export {ReactComponent as delete} from "./delete.svg"
+
