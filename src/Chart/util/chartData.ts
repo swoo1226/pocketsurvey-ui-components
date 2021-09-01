200bdb7dcca7d3d8b4414cd0680256e1dadefd7c
shin-earlysloth <shin@earlysloth.com>
2021-09-01 10:53:39 +0900

    reduce를 돌 때 초기 값을 이중 배열로 설정해서 undefinded가 나오지 않도록 수정

--- src/Chart/util/chartData.ts
+++ src/Chart/util/chartData.ts
@@ -1,7 +1,7 @@
 const zip = (a: any[], b: any[]) =>
   Array.from(Array(Math.min(b.length, a.length)), (_, i) => [a[i], b[i]])
 
-export const ellipsisBarChartData = (
+const ellipsisBarChartData = (
   seriesList: number[],
   labelsList: string[]
 ): {
@@ -13,21 +13,46 @@ export const ellipsisBarChartData = (
   )
   // 0번째 요소에 series, 1번째 요소에 label 값이 들어감
 
-  const others: (string | number)[] =
+  const others: (string | number)[][] =
     seriesList.length <= 14
       ? []
       : chartData.splice(14).reduce(
         (acc, cur) => {
-          (acc[0] as number) += cur[0] as number
+          (acc[0][0] as number) += cur[0] as number
           return acc
         },
-        [0, "그 외"]
+        [[0, "그 외"]]
       )
 
   return {
-    series: [...chartData, others].map((item) => item[0]) as number[],
-    labels: [...chartData, others].map((item) => item[1]) as string[],
+    series: [...chartData, ...others].map((item) => item[0]) as number[],
+    labels: [...chartData, ...others].map((item) => item[1]) as string[],
   }
 }
+
+const series = [
+  0,
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  11,
+  12,
+  13,
+  14,
+  15,
+  16,
+  17,
+  18,
+]
+const labels = series.map((item) => `${item}`)
+console.log(series.length, labels.length)
+ellipsisBarChartData(series, labels)
 
 export default {}
