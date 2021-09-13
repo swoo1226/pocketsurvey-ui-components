83b94cadaf8d8c1ad9a058ebb7aa81662110b5b6
shin-earlysloth <shin@earlysloth.com>
2021-09-13 16:30:09 +0900

    바 차트, 파이 차트 유닛 테스트 작성

--- src/Chart/tests/normalizeChartData.test.ts
+++ src/Chart/tests/normalizeChartData.test.ts
@@ -4,7 +4,7 @@ import {
   twoXData,
   length10Data,
 } from './mocks/chartData';
-import { normalizeBarChartData } from '../util/chartData';
+import { normalizeBarChartData, ellipsisPieChartData } from '../util/chartData';
 
 describe('차트 데이터 정제 테스트', () => {
   describe('가로 막대 차트', () => {
@@ -12,6 +12,9 @@ describe('차트 데이터 정제 테스트', () => {
       const chartData = alphabetData;
       const normalizeData = normalizeBarChartData(chartData.x, chartData.y);
       expect(normalizeData.series.length).toEqual(15);
+      expect(normalizeData.labels[normalizeData.labels.length - 1]).toEqual(
+        '그 외',
+      );
     });
 
     it('선택지에 숫자가 있다면 원래 순서를 반영한다.', () => {
@@ -37,6 +40,24 @@ describe('차트 데이터 정제 테스트', () => {
         JSON.stringify(length10Data.x.sort((a, b) => b - a));
 
       expect(isSorted).toEqual(true);
+    });
+  });
+
+  describe('파이차트 및 Separated 차트', () => {
+    it('차트 데이터가 6개 초과일 때 최대 슬라이스 개수를 6개로 제한하고, 나머지는 그 외로 처리한다.', () => {
+      const normalizeData = ellipsisPieChartData(
+        alphabetData.x,
+        alphabetData.y,
+      );
+      expect(normalizeData.series.length).toEqual(6);
+      expect(normalizeData.labels[normalizeData.labels.length - 1]).toEqual(
+        '그 외',
+      );
+    });
+    it('데이터의 개수가 6개 미만일 때는 그 외를 처리하지 않고 정렬 후 리턴한다.', () => {
+      const normalizeData = ellipsisPieChartData(twoXData.x, twoXData.y);
+      expect(normalizeData.series.sort()).toEqual(twoXData.x.sort());
+      expect(normalizeData.labels.sort()).toEqual(twoXData.y.sort());
     });
   });
 });
