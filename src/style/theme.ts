ac35da7f0727a39a5471d3d2020aa457e7ade205
bree-cheese <bree@earlysloth.com>
2021-09-16 13:26:28 +0900

    color와 typography theme 생성

---
+++ src/style/theme.ts
@@ -0,0 +1,73 @@
+
+const colors = {
+  yellow: "#FAC62D",
+  boldYellow: "#F2AB28",
+  blue: "#59C4DB",
+  boldBlue: "#57ADF2",
+  lightBeige: "#FBFAF8",
+  gray01: "#F0F0F0",
+  gray02: "#DFDEDD",
+  gray03: "#818282",
+  gray04: "#2B2E33",
+  beige: "#E9E1D5",
+  boldBeige: "#D2CBC0",
+  lightYellow: "#FEF4CE",
+  lightBlue: "#DEF3F8",
+  red: "#FF5724",
+  green: "#59BF51",
+}
+
+const typography = {
+  heading1: `
+    font-family: 'Noto Sans CJK KR';
+    font-weight: 400;
+    font-size: 28px;
+    line-height: 150%;
+  `,
+  heading2: `
+    font-family: 'Noto Sans CJK KR;
+    font-weight: 400;
+    font-size: 21px;
+    line-height: 150%;
+  `,
+  subtitle: `
+    font-family: 'Noto Sans CJK KR;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 150%;
+  `,
+  paragraph1: `
+    font-family: 'Noto Sans CJK KR;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 150%;
+  `,
+  paragraph2: `
+    font-family: 'Noto Sans CJK KR';
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 150%;
+  `,
+  number1: `
+    font-family: 'Mont';
+    font-size: 28px;
+    line-height: 150%;
+  `,
+  number2: `
+    font-family: 'Mont';
+    font-size: 18px;
+    line-height: 150%;
+  `,
+  number3: `
+    font-family: 'Mont';
+    font-size: 14px;
+    line-height: 150%;
+  `,
+}
+
+const theme = {
+  colors,
+  typography,
+}
+
+export default theme
