90c0c3c0a7ec2422c3a6434326013efdb0ed7ca2
pp.jo <pp.jo@earlysloth.com>
2021-09-16 11:32:02 +0900

    ui 컴포넌트 로고 추가

---
+++ .storybook/pks-theme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'My custom storybook',
+  brandUrl: 'https://home.pocketsurvey.co.kr/',
+  brandImage: 'https://s3.ap-northeast-2.amazonaws.com/pocketsurvey.earlysloth/images/public/logo.png',
+});
