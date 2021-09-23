b5053142ea93493086e4f771a426eb8c142de5f9
ellie-hahm <ellie.hahm@earlysloth.com>
2021-09-23 14:56:44 +0900

    사용방법 생략

--- src/Tag/Tag.stories.tsx
+++ src/Tag/Tag.stories.tsx
@@ -30,16 +30,10 @@ export function Index() {
   return (
     <>
     <Discript>
-    <h2>사용 방법</h2>
-    필수로 넘겨야 하는 타입은 theme으로 아래와 같이 3가지가 있습니다. <br></br>
      <Tag theme="pastellBlue" disabled={true}>pastellBlue </Tag>
      <Tag theme="pastellYellow" disabled={true}>pastellYellow </Tag>
-     <Tag theme="defaultGray" disabled={true}>defaultGray </Tag> <br></br>
-    그 외의 색상을 사용할 경우, 아래처럼 backgroundColor 속성에 색상을 넣어주면됩니다. <br></br>
+     <Tag theme="defaultGray" disabled={true}>defaultGray </Tag>
     <Tag theme="defaultGray" backgroundColor={"#fac62d"} disabled={true}>#fac62d </Tag><br></br>
-    disabled속성을 True로 넘기면 onClick 속성을 추가하지 않고, false로 사용하면 onClick 속성을 사용할 수 있습니다 <br></br>
-    hover시 색상변경을 하려면 hoverColor 속성에 값을 추가하면 됩니다. <br></br>
-    fontSize 속성은 디폴트로 12px이고 따로 지정이 가능합니다.
 
     </Discript>
       <ConditionAlert>
