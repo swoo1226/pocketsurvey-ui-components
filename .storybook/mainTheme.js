7e6c00699f76383e94602f0dc4bf28679187acb0
ellie-hahm <ellie.hahm@earlysloth.com>
2021-09-24 13:17:21 +0900

    스토리북에 로고 추가

---
+++ .storybook/mainTheme.js
@@ -0,0 +1,8 @@
+import { create } from "@storybook/theming"
+import mainLogo from "./mainLogo.svg"
+
+export default create({
+  base: "light",
+  brandUrl: "https://github.com/earlysloth-devteam/pocketsurvey-ui-components",
+  brandImage: mainLogo,
+})
