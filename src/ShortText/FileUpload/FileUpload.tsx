d1aa136510303ce072769aac1e2f10d899af7ae3
ellie-hahm <ellie.hahm@earlysloth.com>
2021-09-29 10:18:29 +0900

    업로드 전, 후에 대한 stories 예시 추가

--- src/ShortText/FileUpload/FileUpload.tsx
+++ src/ShortText/FileUpload/FileUpload.tsx
@@ -1,25 +1,55 @@
-import React, { useState, useCallback } from "react"
+import React, { useState, useEffect, useCallback } from "react"
 import styled from "styled-components"
 import { useDropzone } from "react-dropzone"
 import Icon from "../../Icon/Icon"
+import loadingSpinner from "../../Icon/svg/loadingSpinner.svg"
 
 const Container = styled.div``
-const UploadWrapper = styled.div`
-  border: 2px dashed #dfdedd;
+const UploadWrapper = styled.div<{ answeredText: string | null }>`
+  border: ${(props) => (props.answeredText ? "" : "2px dashed #dfdedd")};
   border-radius: 5px;
   width: 655px;
   padding-bottom: 28px;
   .loadingSpinner {
-    width: 100%;
-    height: 60px;
-    margin-top: 28px;
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
     flex-direction: column;
+    padding-top: 30px;
     span {
       font-size: 14px;
       color: #818282;
       font-family: "Noto Sans CJK KR Regular";
+      margin-top: 20px;
+    }
+    img {
+      color: #fac62d;
+      width: calc(1.7 * 14px);
+      height: calc(1.2 * 14px);
+      -webkit-animation: spin 1s linear infinite;
+      -moz-animation: spin 1s linear infinite;
+      animation: spin 1s linear infinite;
+    }
+    @-moz-keyframes spin {
+      100% {
+        -moz-transform: rotate(360deg);
+      }
+    }
+    @-webkit-keyframes spin {
+      100% {
+        -webkit-transform: rotate(360deg);
+      }
+    }
+    @keyframes spin {
+      100% {
+        -webkit-transform: rotate(360deg);
+        transform: rotate(360deg);
+      }
     }
   }
+
   .deleteBtn {
     margin-top: 20px;
   }
@@ -72,39 +102,67 @@ const FileUploadIcon = styled.div`
   text-align: center;
   padding-top: 28px;
 `
+const UploadResult = styled.div`
+  width: 100%;
+  .file-row {
+    cursor: pointer;
+    height: 34px;
+    display: flex;
+    justify-content: space-between;
+    padding: 0px 14px;
+    color: #2b2e33;
+    font-family: "Noto Sans CJK KR Regular";
+    font-size: 14px;
+    line-height: 23.8px;
+    border-radius: 3px;
+    .check,
+    .filename {
+      margin-top: 6px;
+    }
+    .deleteFile {
+      margin-top: 8px;
+      padding-right: 15px;
+    }
+    .filename {
+      width: 614px;
+      height: 20px;
+    }
+    .check {
+      padding-right: 7px;
+    }
+    background-color: #f0f0f0;
+  }
+  video {
+    width: 80%;
+  }
+`
 
 export type FileUploadTypes = {
-  loading: boolean;
   qrCode: [string, string][] | [];
-  id: string;
-  answeredText: string | null;
-  questionIndex: number;
+  answeredText: string ;
+  onCancelClick?: () => void;
 };
 
 function FileUpload({
-  loading,
   qrCode,
-  id,
   answeredText,
-  questionIndex,
+  onCancelClick,
 }: FileUploadTypes): JSX.Element {
+  
+  useEffect(() => {
+    console.log("answeredText", answeredText)
+  }, [answeredText])
+
   const [isLoading, setIsLoading] = useState<boolean>(false)
-  console.log(loading, qrCode, id)
+  const [uploadedFile, setUploadedFile] = useState<string>("")
+
+  useEffect(() => {
+    if(answeredText) {
+      setUploadedFile(answeredText)
+    }
+  }, [answeredText])
   const onDrop = useCallback((acceptedFiles) => {
-    // if (validationFileExtension(fileName, subType)) {
-    //   setLoading(true);
-    //   uploadFile({
-    //     file: acceptedFiles[0],
-    //     questionIndex,
-    //   });
-    // } else {
-    //   setLoading(false);
-    //   window.alert(
-    //     `다음 확장자만 업로드가 가능합니다.\n${switchAccept(subType)
-    //       .replace(/\./g, '')
-    //       .replace(/image\//gi, '')}`,
-    //   );
-    // }
+    uploadValidation(acceptedFiles[0])
   }, [])
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
@@ -114,15 +172,27 @@ function FileUpload({
   const fileAcceptance =
     ".pdf,.doc,.docx,.hwp,.xls,.xlsx,.ppt,.pptx,.zip,.alz,.rar,.7z"
 
-  const validationFileExtension = (fileName: string) => {
+  const uploadValidation = (file: File) => {
+    const fileName = file.name
+    console.log("fileName", fileName)
+    if (isValidFile(fileName)) {
+      setIsLoading(true)
+    } else {
+      setIsLoading(false)
+      window.alert(
+        `다음 확장자만 업로드가 가능합니다.\n${fileAcceptance
+          .replace(/\./g, "")
+          .replace(/image\//gi, "")}`
+      )
+    }
+  }
+  const isValidFile = (fileName: string) => {
+    const accept = fileAcceptance.replace(/\./gi, "").split(",")
     const extension = fileName.substring(
       fileName.lastIndexOf(".") + 1,
       fileName.length
     )
-    const accept = fileAcceptance.replace(/\./gi, "").split(",")
-    if (accept.includes(extension)) {
-      return true
-    }
+    if (accept.includes(extension)) return true
     return false
   }
 
@@ -130,62 +200,54 @@ function FileUpload({
     <>
       <UploadWrapper
         {...getRootProps()}
-        answeredText="Example"
+        answeredText={uploadedFile}
         isDragActive={isDragActive}
       >
-        {loading ? (
-          <BeforeUpload>
-            <QRBox>
-              {qrCode !== [] ? (
-                <img
-                  src={`data:image/png;base64, ${qrCode[1]}`}
-                  style={{ width: "100%" }}
-                  alt="alt"
-                />
-              ) : (
-                <div className="noQR">
-                  {" "}
-                  QR 코드가 <br />
-                  준비중입니다
-                </div>
-              )}
-            </QRBox>
-          </BeforeUpload>
+        {isLoading && !uploadedFile ? (
+          <div className="loadingSpinner">
+            <img alt="loading" src={loadingSpinner} />
+            <span> 파일을 업로드중입니다...</span>
+          </div>
+        ) : uploadedFile && !isLoading ? (
+          <UploadResult>
+            <div className="file-row">
+              <div className="filename">{uploadedFile}</div>
+              <Icon
+                icon="exit"
+                width={20}
+                className="deleteFile"
+                color="#818282"
+                onClick={() => {
+                  onCancelClick
+                  setUploadedFile("")
+                }}
+                useCursor
+              />
+            </div>
+          </UploadResult>
         ) : (
-          <FileUploadIcon>
-            <Icon icon="fileUpload" width={97.42} color="#FAC62D" />
-          </FileUploadIcon>
+          <UploadText>
+            <FileUploadIcon>
+              <Icon icon="fileUpload" width={97.42} color="#FAC62D" />
+            </FileUploadIcon>
+            <StressLetter> 여기에 파일을 끌어서 놓기</StressLetter> 또는
+            <FileUploadLabel>
+              <span> 브라우저에서</span>
+              <input
+                {...getInputProps()}
+                className="fileBtn"
+                type="file"
+                accept={fileAcceptance}
+                style={{ display: "none" }}
+                onChange={(e) => {
+                  uploadValidation(e.target.files![0])
+                }}
+              />
+            </FileUploadLabel>
+            <span> 올리기</span>
+            <div className="file-size-text">최대 50MB 가능</div>
+          </UploadText>
         )}
-        <UploadText>
-          <StressLetter> 여기에 파일을 끌어서 놓기</StressLetter> 또는
-          <FileUploadLabel htmlFor={`fileUpload-${questionIndex}`}>
-            <span> 브라우저에서</span>
-            <input
-              {...getInputProps()}
-              className="fileBtn"
-              type="file"
-              id={`fileUpload-${questionIndex}`}
-              accept={fileAcceptance}
-              style={{ display: "none" }}
-              onChange={(e) => {
-                const fileName = e.target.files![0].name
-                const accept = fileAcceptance.replace(/\./gi, "").split(",")
-                if (validationFileExtension(fileName)) {
-                  loading = true
-                } else {
-                  setIsLoading(false)
-                  window.alert(
-                    `다음 확장자만 업로드가 가능합니다.\n${fileAcceptance
-                      .replace(/\./g, "")
-                      .replace(/image\//gi, "")}`
-                  )
-                }
-              }}
-            />
-          </FileUploadLabel>
-          <span> 올리기</span>
-          <div className="file-size-text">최대 50MB 가능</div>
-        </UploadText>
       </UploadWrapper>
     </>
   )
