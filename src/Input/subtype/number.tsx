2583d881e6e6af813f779e6187c457ee739185b0
jjun <jjun@earlysloth.com>
2021-10-29 16:55:10 +0900

    number x아이콘 추가

--- src/Input/subtype/number.tsx
+++ src/Input/subtype/number.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import Input from '../Input';
+import Icon from '../../Icon/Icon';
 
 type NumberPropsType = {
   value: string;
@@ -10,56 +11,103 @@ type NumberPropsType = {
 
 function Number({ value, onChange, isMobile }: NumberPropsType) {
   const [errorMessage, setErrorMessage] = useState<string>('');
+  const [isHover, setIsHover] = useState<boolean>(false);
+
+  const onMobileInputChange = (innerValue: string) => {
+    if (/^[+-]?\d*(\.?\d*)$/.test(innerValue)) {
+      onChange(innerValue);
+      setErrorMessage('');
+    } else {
+      setErrorMessage('숫자만 입력 가능합니다.');
+    }
+  };
+
+  const onInputChange = (innerValue: string) => {
+    if (/^[+-]?\d*(\.?\d*)$/.test(innerValue)) {
+      onChange(innerValue);
+      setErrorMessage('');
+    } else {
+      setErrorMessage('숫자만 입력 가능합니다.');
+    }
+  };
+
+  const onResetButtonClick = () => {
+    onChange('');
+  };
 
   if (isMobile) {
     return (
-      <Input
-        inputMode="decimal"
-        type="text"
-        mode="basic"
-        width={300}
-        fullWidthMode
-        fontSize={14}
-        isError={!!errorMessage}
-        errorMessage={errorMessage}
-        borderColor="#FAC609"
-        placeholder="숫자를 입력해주세요"
-        pattern="[0-9]*"
-        value={value}
-        onChange={(innerValue: string) => {
-          if (/^[+-]?\d*(\.?\d*)$/.test(innerValue)) {
-            onChange(innerValue);
-            setErrorMessage('');
-          } else {
-            setErrorMessage('숫자만 입력 가능합니다.');
-          }
-        }}
-        ignorePlaceholderColor
-      />
+      <Wrapper>
+        <Input
+          inputMode="decimal"
+          type="text"
+          mode="basic"
+          width={300}
+          fullWidthMode
+          fontSize={14}
+          isError={!!errorMessage}
+          errorMessage={errorMessage}
+          borderColor="#FAC609"
+          placeholder="숫자를 입력해주세요"
+          pattern="[0-9]*"
+          value={value}
+          onChange={onMobileInputChange}
+          ignorePlaceholderColor
+        />
+        {value.length > 0 ? (
+          <ResetButton
+            icon={isHover ? 'titleInputXHover' : 'titleInputX'}
+            onMouseOver={() => setIsHover(true)}
+            onMouseLeave={() => setIsHover(false)}
+            width={18}
+            color="black"
+            onClick={onResetButtonClick}
+            isMobile
+          />
+        ) : null}
+      </Wrapper>
     );
   }
 
   return (
-    <Input
-      mode="basic"
-      width={329}
-      isError={!!errorMessage}
-      errorMessage={errorMessage}
-      borderColor="#FAC609"
-      placeholder="숫자를 입력해주세요"
-      value={value}
-      fontSize={14}
-      onChange={(innerValue: string) => {
-        if (/^[+-]?\d*(\.?\d*)$/.test(innerValue)) {
-          onChange(innerValue);
-          setErrorMessage('');
-        } else {
-          setErrorMessage('숫자만 입력 가능합니다.');
-        }
-      }}
-      ignorePlaceholderColor
-    />
+    <Wrapper>
+      <Input
+        mode="basic"
+        width={329}
+        isError={!!errorMessage}
+        errorMessage={errorMessage}
+        borderColor="#FAC609"
+        placeholder="숫자를 입력해주세요"
+        value={value}
+        fontSize={14}
+        onChange={onInputChange}
+        ignorePlaceholderColor
+      />
+      {value.length > 0 ? (
+        <ResetButton
+          icon={isHover ? 'titleInputXHover' : 'titleInputX'}
+          onMouseOver={() => setIsHover(true)}
+          onMouseLeave={() => setIsHover(false)}
+          width={18}
+          color="black"
+          onClick={onResetButtonClick}
+          isMobile={false}
+        />
+      ) : null}
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  position: relative;
+`;
+
+const ResetButton = styled(Icon)<{
+  isMobile: boolean;
+}>`
+  position: absolute;
+  top: 12px;
+  ${(props) => (props.isMobile ? 'right: -20px;' : 'left: 330px;')}
+`;
 
 export default Number;
