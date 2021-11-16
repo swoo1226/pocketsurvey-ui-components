dbed7e085bdd13b498bdc66b026c26302be80e68
Ellie(Nayoun)Ham <ellie.hahm0502@gmail.com>
2021-11-16 14:03:17 +0900

    ExplainedModal 수정

--- src/Modal/ExplainedModal.stories.tsx
+++ src/Modal/ExplainedModal.stories.tsx
@@ -15,30 +15,36 @@ const discript =
   '설명 텍스트 여기 설명 텍스트 여기 설명 텍스트 여기 설명 텍스트 여기 설명 텍스트 여기 설명 텍스트 여기 설명 텍스트 여기 설명 텍스트 여기설명 텍스트 여기 설명 텍스트 여기 설명 텍스트 여기 설명 텍스트 여기설명 텍스트 여기 설명 텍스트 여기 설명 텍스트 여기 설명 텍스트 여기';
 
 const Wrapper = styled.div`
-    background-color: #E5E5E5;
-    display: flex;
-    justify-content: center;
-    justify-content: space-between;
+  background-color: #e5e5e5;
+  display: flex;
+  justify-content: center;
+  justify-content: space-between;
 `;
 export function Index() {
   return (
     <Wrapper>
-    <ExplainedModal
-      img={chunsic}
-      discript={discript}
-      isCallOut={true}
-      onMoreClick={() => console.log('네! 더볼래요')}
-      onFinishClick={() => console.log('네! 이해했어요!')}
-      callOutText={callOutText}
-    ></ExplainedModal>
-     <ExplainedModal
-      img={chunsic}
-      discript={discript}
-      isCallOut={false}
-      onMoreClick={() => console.log('네! 더볼래요')}
-      onFinishClick={() => console.log('네! 이해했어요!')}
-      callOutText={callOutText}
-    ></ExplainedModal>
+      <ExplainedModal
+        title="설명 제목"
+        img={chunsic}
+        discript={discript}
+        isCallOut={true}
+        onLeftClick={() => alert('네! 더볼래요')}
+        onRightClick={() => alert('네! 이해했어요!')}
+        callOutText={callOutText}
+        tagColor={'#fac62d'}
+        tagText="사용Tip"
+      ></ExplainedModal>
+      <ExplainedModal
+        title="설명 제목2"
+        img={chunsic}
+        discript={discript}
+        isCallOut={false}
+        onLeftClick={() => alert('네! 더볼래요')}
+        onRightClick={() => alert('네! 이해했어요!')}
+        callOutText={callOutText}
+        tagText="이용 Tip"
+        tagColor={'#59C4DB'}
+      ></ExplainedModal>
     </Wrapper>
   );
 }
