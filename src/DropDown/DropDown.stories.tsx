d59ff82cc3e36ec5dbf682cf834dedb0eba2699a
shin-earlysloth <shin@earlysloth.com>
2021-12-20 18:02:43 +0900

    드롭다운 스타일 분리 및 오류 처리 추가

--- src/DropDown/DropDown.stories.tsx
+++ src/DropDown/DropDown.stories.tsx
@@ -1,13 +1,18 @@
-import React, { useState } from "react";
-import { withKnobs, boolean, select, color, number } from "@storybook/addon-knobs";
-import styled from "styled-components";
-import DropDown from "./DropDown";
-import Font from "../Typography/Font";
-import { Meta } from "@storybook/react/types-6-0";
-
+import React, { useState } from 'react';
+import {
+  withKnobs,
+  boolean,
+  select,
+  color,
+  number,
+} from '@storybook/addon-knobs';
+import styled from 'styled-components';
+import DropDown from './DropDown';
+import Font from '../Typography/Font';
+import { Meta } from '@storybook/react/types-6-0';
 export default {
   component: DropDown,
-  title: "Components/DropDown",
+  title: 'Components/DropDown',
   decorators: [withKnobs], // 애드온 적용
 } as Meta;
 
@@ -18,13 +23,14 @@ const DropDownContainer = styled.div`
 export function Index() {
   const [selected, setSelected] = useState(0);
 
-  const disabled = boolean("disabled", false);
+  const disabled = boolean('disabled', false);
 
-  const mainColor = color("bold color", "#FAC62D");
-  const subColor = color("light color", "#fef4ce");
+  const mainColor = color('bold color', '#FAC62D');
+  const subColor = color('light color', '#fef4ce');
 
   return (
     <>
+      <button onClick={() => setSelected(100)}>selected를 100으로 설정</button>
       <Font
         fontFace="Noto Sans CJK KR"
         fontWeight="500"
@@ -54,16 +60,20 @@ export function Index() {
         <DropDown
           list={[
             {
-              selectionName: "객관식 (단일 선택)",
-              icon: "singleChoice",
+              selectionName: '객관식 (단일 선택)',
+              icon: 'singleChoice',
             },
-            { selectionName: "객관식 (복수 선택)", icon: "singleChoice" },
-            { selectionName: "객관식 (이미지 선택)", icon: "singleChoice" },
-            { selectionName: "주관식 (텍스트)", icon: "singleChoice" },
-            { selectionName: "주관식 (이미지)", icon: "singleChoice" },
-            { selectionName: "객관식 (영상)", icon: "singleChoice" },
-            { selectionName: "설명 추가", icon: "singleChoice", hidden: true },
-            { selectionName: "순위 설정", icon: "singleChoice" },
+            { selectionName: '객관식 (복수 선택)', icon: 'singleChoice' },
+            { selectionName: '객관식 (이미지 선택)', icon: 'singleChoice' },
+            { selectionName: '주관식 (텍스트)', icon: 'singleChoice' },
+            { selectionName: '주관식 (이미지)', icon: 'singleChoice' },
+            { selectionName: '객관식 (영상)', icon: 'singleChoice' },
+            {
+              selectionName: '설명 추가',
+              icon: 'singleChoice',
+              hidden: true,
+            },
+            { selectionName: '순위 설정', icon: 'singleChoice' },
           ]}
           selected={selected}
           disable={disabled}
@@ -110,24 +120,24 @@ export function Index() {
 function BranchSelector() {
   const [selected, setSelected] = useState(null);
 
-  const disabled = boolean("disabled", false);
+  const disabled = boolean('disabled', false);
 
-  const mainColor = color("bold color", "#59C4DB");
-  const subColor = color("light color", "#DEF3F8");
+  const mainColor = color('bold color', '#59C4DB');
+  const subColor = color('light color', '#DEF3F8');
 
   return (
     <DropDown
-      placeholder={"선택해주세요"}
+      placeholder={'선택해주세요'}
       list={[
-        { selectionName: "다음 문항" },
-        { selectionName: "Q1. 일이삼오육칠팔구십" },
-        { selectionName: "Q2. 일이삼오육칠팔구십" },
-        { selectionName: "Q3. 일이삼오육칠팔구십" },
-        { selectionName: "Q4. 일이삼오육칠팔구십" },
-        { selectionName: "Q5. 일이삼오육칠팔구십" },
-        { selectionName: "Q6. 일이삼오육칠팔구십" },
-        { selectionName: "Q7. 일이삼오육칠팔구십" },
-        { selectionName: "종료메시지로" },
+        { selectionName: '다음 문항' },
+        { selectionName: 'Q1. 일이삼오육칠팔구십' },
+        { selectionName: 'Q2. 일이삼오육칠팔구십' },
+        { selectionName: 'Q3. 일이삼오육칠팔구십' },
+        { selectionName: 'Q4. 일이삼오육칠팔구십' },
+        { selectionName: 'Q5. 일이삼오육칠팔구십' },
+        { selectionName: 'Q6. 일이삼오육칠팔구십' },
+        { selectionName: 'Q7. 일이삼오육칠팔구십' },
+        { selectionName: '종료메시지로' },
       ]}
       selected={selected}
       disable={disabled}
@@ -138,56 +148,54 @@ function BranchSelector() {
   );
 }
 
-const PSection = (title : string) => 
-<p
-  key={title}
-  style={{
-    fontSize: "14px",
-    fontWeight: 500,
-    margin: "21px 14px",
-  }}
->
-  {title}
-</p>;
+const PSection = (title: string) => (
+  <p
+    key={title}
+    style={{
+      fontSize: '14px',
+      fontWeight: 500,
+      margin: '21px 14px',
+    }}
+  >
+    {title}
+  </p>
+);
 
 function HrSelector() {
   const [selected, setSelected] = useState(0);
 
-  const disabled = boolean("disabled", false);
+  const disabled = boolean('disabled', false);
 
-  const mainColor = color("bold color", "#59C4DB");
-  const subColor = color("light color", "#DEF3F8");
+  const mainColor = color('bold color', '#59C4DB');
+  const subColor = color('light color', '#DEF3F8');
 
-  const target1 = number("target1", 0)
-  const target2 = number("target2", 3)
+  const target1 = number('target1', 0);
+  const target2 = number('target2', 3);
 
-
-  
   return (
     <DropDown
-      placeholder={"선택해주세요"}
+      placeholder={'선택해주세요'}
       list={[
-        { selectionName: "다음 문항" },
-        { selectionName: "Q1. 일이삼오육칠팔구십" },
-        { selectionName: "Q2. 일이삼오육칠팔구십" },
-        { selectionName: "Q3. 일이삼오육칠팔구십" },
-        { selectionName: "Q4. 일이삼오육칠팔구십" },
-        { selectionName: "Q5. 일이삼오육칠팔구십" },
-        { selectionName: "Q6. 일이삼오육칠팔구십" },
-        { selectionName: "Q7. 일이삼오육칠팔구십" },
-        { selectionName: "종료메시지로" },
+        { selectionName: '다음 문항' },
+        { selectionName: 'Q1. 일이삼오육칠팔구십' },
+        { selectionName: 'Q2. 일이삼오육칠팔구십' },
+        { selectionName: 'Q3. 일이삼오육칠팔구십' },
+        { selectionName: 'Q4. 일이삼오육칠팔구십' },
+        { selectionName: 'Q5. 일이삼오육칠팔구십' },
+        { selectionName: 'Q6. 일이삼오육칠팔구십' },
+        { selectionName: 'Q7. 일이삼오육칠팔구십' },
+        { selectionName: '종료메시지로' },
       ]}
       hrs={[
         {
           targetIndex: target1,
-          title: "첫번째 구간 시작",
-          section: PSection("첫번째 구간 시작")
-          ,
+          title: '첫번째 구간 시작',
+          section: PSection('첫번째 구간 시작'),
         },
         {
           targetIndex: target2,
-          title: "두번째 구간 시작",
-          section: PSection("두번째 구간 시작")
+          title: '두번째 구간 시작',
+          section: PSection('두번째 구간 시작'),
         },
       ]}
       id="hrDropDown"
---
+++ src/DropDown/styles.ts
@@ -0,0 +1,113 @@
+import styled from 'styled-components';
+
+export const PNG = styled.img<{ pngImageCropCircle?: boolean }>`
+  width: 23px !important;
+  height: 21px !important;
+  margin-bottom: 0px !important;
+  ${(props) =>
+    props.pngImageCropCircle && 'object-fit:cover; border-radius:50%;'}
+`;
+
+export const DropDownContainer = styled.div<{
+  width: number;
+  containerHeight?: string;
+}>`
+  width: ${(props) => props.width}px;
+  font-size: 12px;
+  text-align: left;
+  ${(props) => props.containerHeight && `height: ${props.containerHeight};`}
+`;
+
+export const DropDownBoxContainer = styled.div<{
+  disable: boolean;
+  themeColor: string;
+  zIndex: number;
+  height: number;
+}>`
+  width: 100%;
+  height: ${(props) => props.height}px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  box-sizing: border-box;
+  padding: 0 10px;
+  border-radius: 3px;
+  border: 1px solid ${(props) => (props.disable ? '#F0F0F0' : props.themeColor)};
+  background-color: ${(props) => (props.disable ? '#F0F0F0' : 'white')};
+  cursor: ${(props) => (props.disable ? 'no-drop' : 'pointer')};
+  z-index: ${(props) => props.zIndex - 1};
+`;
+
+export const DropDownBox = styled.div<{
+  disable: boolean;
+  themeColor: string;
+  height: number;
+}>`
+  width: 80%;
+  height: ${(props) => props.height}px;
+  display: flex;
+  align-items: center;
+  color: ${(props) => (props.disable ? '#818282' : '#111111')};
+  background-color: ${(props) => (props.disable ? '#F0F0F0' : undefined)};
+  cursor: ${(props) => (props.disable ? 'no-drop' : 'pointer')};
+`;
+
+export const DropDownList = styled.div<{
+  disable: boolean;
+  isShowList: boolean;
+  listLength: number;
+  width: number;
+  zIndex: number;
+  height: number;
+  listMaxHeight?: number;
+  textColor?: string;
+  fontSize?: number;
+}>`
+  width: ${(props) => props.width}px;
+  z-index: ${(props) => props.zIndex};
+  height: ${(props) => props.listLength * props.height}px;
+  position: absolute;
+  box-shadow: 0px 3px 6px #d2cbc0;
+  color: ${(props) =>
+    props.disable ? props.textColor ?? '#818282' : '#111111'};
+  border-radius: 3px;
+  padding: 8px 0;
+  max-height: ${(props) => `${props.listMaxHeight ?? 200}px`};
+  overflow-y: auto;
+  background: #ffffff;
+  margin: 7px 0px;
+  ${(props) => props.fontSize && `font-size: ${props.fontSize}px;`}
+`;
+
+export const DropDownItem = styled.div<{
+  index: number;
+  selected: number;
+  themeColor: string;
+  hidden?: boolean;
+  height: number;
+}>`
+  display: ${(props) => (props.hidden ? 'none' : 'flex')};
+  align-items: center;
+  box-sizing: border-box;
+  padding: 0 10px;
+  height: ${(props) => props.height}px;
+  cursor: pointer;
+  background-color: ${(props) =>
+    props.selected === props.index ? '#F0F0F0' : 'white'};
+  cursor: pointer;
+  &:hover {
+    background-color: ${(props) =>
+      props.selected === props.index ? '#F0F0F0' : props.themeColor};
+  }
+`;
+
+export const DropDownItemText = styled.p<{ fontSize?: number }>`
+  white-space: nowrap;
+  overflow: hidden;
+  align-items: center;
+  width: 100%;
+  margin: 0;
+  margin-left: 6px;
+  text-overflow: ellipsis;
+  ${(props) => props.fontSize && `font-size: ${props.fontSize}px;`}
+`;
